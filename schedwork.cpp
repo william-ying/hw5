ccd434da7b20b9c6189cc41ed74c9207e9fb1d5c
William Wang <61264396+william-ying@users.noreply.github.com>
2025-12-02 23:31:49 -0800

    Update schedwork.cpp

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,18 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+void perms(int index, int dailyNeed, vector<Worker_T> &data, vector<vector<Worker_T>> &ret, vector<Worker_T> rowavail) {
+    if (data.size() == dailyNeed) {
+        ret.push_back(data);
+        return;
+    }
+    for (int i = index; i < rowavail.size(); i++) {
+        data.push_back(rowavail[i]);
+        perms(i + 1, dailyNeed, data, ret, rowavail);
+        data.pop_back();
+    }
+}
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,11 +47,36 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
-    sched.clear();
+    // sched.clear();
     // Add your code below
-
-
-
-
+    vector<Worker_T> rowavail;
+    for (int i = 0; i < avail[0].size(); i++) {
+        if (avail[0][i]) {
+            bool add = 0;
+            for (int j = 0; j < sched.size(); j++) {
+                if (sched[j].find(sched[j].begin(), sched[j].end(), i) != sched[j].end) add += 1;
+            }
+            if (add < maxShifts) {
+                rowavail.push_back(i);
+            }
+        }
+    }
+    if (rowavail.size() < dailyNeed) return false;
+    vector<vector<Worker_T>> schedrows;
+    vector<Worker_T> data;
+    perms(0, dailyNeed, data, schedrows, rowavail);
+    if (schedrows.size() > 0 && avail.size() == 1) {
+        sched.push_back(schedrows[0]);
+        return true;
+    }
+    for (int i = 0; i < schedrows.size(); i++) {
+        vector<bool> tempavail = avail[0];
+        avail.erase(avail.begin());
+        sched.push_back(schedrows[i]);
+        if (schedule(avail, dailyNeed, maxShifts, sched)) return true;
+        sched.pop_back();
+        avail.insert(avail.begin(), tempavail);
+    }
+    return false;
 }
 
